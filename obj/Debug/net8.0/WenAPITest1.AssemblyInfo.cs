a45691a1788831819fbf5627164efdf92d2d4196
rakesh.ganneboina <rakesh.ganneboina@gmail.com>
2024-12-31 23:39:03 +0530

    Changes in feature1 branch

--- obj/Debug/net8.0/WenAPITest1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WenAPITest1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WenAPITest1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8632f083f738e6e69feeaafc6a66639569b42a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WenAPITest1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WenAPITest1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
