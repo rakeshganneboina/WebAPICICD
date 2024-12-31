d9e4f25f3ea56da7c708afae4b3e6fcbf30c555a
rakesh.ganneboina <rakesh.ganneboina@gmail.com>
2024-12-31 19:35:13 +0530

    First change and push

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -1,5 +1,12 @@
 namespace WenAPITest1
 {
+    //git init
+    //git add.
+    //git commit -m "Initial commit"
+    //git remote add origin <repository-url>
+    //git branch -M main
+    //git push -u origin main
+
     public class WeatherForecast
     {
         public DateOnly Date { get; set; }
